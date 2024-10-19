61296d34d9eef5cc9aa5f82b812c6f5969dac3ec
prnsydv <princ9861@gmail.com>
2024-10-19 08:04:39 +0530

    added the profile view

---
+++ script_listMentor.js
@@ -0,0 +1,33 @@
+// Fetching mentor data
+fetch("db.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // id fetch
+    const mentorDataContainer = document.getElementById(
+      "mentor-data-container"
+    );
+
+    // data fetch
+    const mentorData = data.mentor_ListData;
+
+    // mentor data html
+    mentorData.forEach((mentor) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <img src="${mentor.image}" alt="${mentor.name}" class="card--image" />
+        <div class="card--content">
+          <div class="profile">
+            <span>${mentor.name}</span>
+            <span>${mentor.position}</span>
+          </div>
+          <a class="view--btn" style="text-decoration:none" href="${mentor.linkToViewPage}">View</a>
+        </div>`;
+        
+
+      mentorDataContainer.appendChild(card);
+    });
+
+  })
+  .catch((error) => console.error("Error fetching mentor data:", error));
